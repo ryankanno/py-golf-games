571233bdcce86ca0e96c17fa66a0c7706c3a8cc9
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-29 07:58:23 -0400

    Added tests to Round object Added concept of current_hole to Round Added abstract methods to Game

--- py_golf/games/game.py
+++ py_golf/games/game.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import abc
+
 
 class Game(object):
-    def __init__(self, name, *args, **kwargs):
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, name, players, *args, **kwargs):
         super(Game, self).__init__(*args, **kwargs)
         self._name = name
+        self._players = {}
+        for player in players:
+            self._players[player] = player
 
     @property
     def name(self):
         return self._name
 
+    @property
+    def players(self):
+        return self._players.values()
+
+    @property
+    def is_participating(self, player):
+        return player in self._players.values()
+
+    @abc.abstractmethod
     def pre_round(self):
-        pass
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def pre_hole(self):
-        pass
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def at_turn(self):
-        pass
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def post_hole(self):
-        pass
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def post_round(self):
-        pass
+        raise NotImplementedError
 
 # vim: filetype=python
--- py_golf/round.py
+++ py_golf/round.py
@@ -5,11 +5,28 @@ from scorecard import Scorecard
 
 
 class Round(object):
+    """
+    >>> from player import Gender, Player
+    >>> p1 = Player("SpongeBob", Gender.M, 10)
+    >>> p2 = Player("Squarepants", Gender.F, 10)
+    >>> from course import Course
+    >>> course = Course("Ocean")
+    >>> round = Round(course, [p1, p2])
+    >>> course is round.course
+    True
+    >>> len(round.players)
+    2
+    >>> p3 = Player("Joe", Gender.M, 10)
+    >>> round.add_player(p3)
+    >>> len(round.players)
+    3
+    """
     def __init__(self, course, players=[], *args, **kwargs):
         super(Round, self).__init__(*args, **kwargs)
         self._course = course
         self._players = players
         self._scorecard = Scorecard()
+        self._current_hole = None
 
     @property
     def course(self):
@@ -23,8 +40,16 @@ class Round(object):
     def scorecard(self):
         return self._scorecard
 
+    @property
+    def current_hole(self):
+        return self._current_hole
+
+    @current_hole.setter
+    def current_hole(self, value):
+        self._current_hole = value
+
     def add_player(self, player):
-        self._players.push(player)
+        self._players.append(player)
 
     def record_score(self, player, hole_number, score):
         if player in self._players and self.course.get_hole(hole_number):
