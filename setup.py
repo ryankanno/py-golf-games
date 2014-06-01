bcc713fdde011e58b09181a760d66dd70e68ae74
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-01 10:34:47 -0400

    Removed py_golf dependency from setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
-import py_golf
 
 packages = [
     'py_golf',
@@ -14,13 +15,47 @@ packages = [
     'py_golf.simulation',
 ]
 
+here = os.path.dirname(os.path.realpath(__file__))
+
+# Metadata
+
+meta = {}
+re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
+re_version = re.compile(r'VERSION\s*=.*?\((.*?)\)')
+strip_quotes = lambda s: s.strip("\"'")
+
+
+def add_version(match):
+    return {'VERSION': match.group(1).replace(" ", "").replace(",", ".")}
+
+
+def add_meta(match):
+    attr_name, attr_value = m.groups()
+    return {attr_name: strip_quotes(attr_value)}
+
+
+patterns = {
+    re_meta: add_meta,
+    re_version: add_version
+}
+
+
+with open(os.path.join(here, 'py_golf/__init__.py'), 'r') as f:
+    for line in f:
+        for pattern, handler in patterns.items():
+            m = pattern.match(line.strip())
+            if m:
+                meta.update(handler(m))
+
+# Requires
+
 requires = ['enum34']
 tests_require = ['flake8', 'mock', 'nose', 'nosexcover']
 
-with open('README.rst') as f:
+with open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
 
-with open('CHANGES') as f:
+with open(os.path.join(here, 'CHANGES')) as f:
     changes = f.read()
 
 classifiers = [
@@ -32,17 +67,17 @@ classifiers = [
 
 setup(
     name='py-golf',
-    version=py_golf.__version__,
+    version=meta['VERSION'],
     description='Tiny golfing simulation',
     long_description=readme + '\n\n' + changes,
-    author=py_golf.__author__,
-    author_email='ryankanno@localkinegrinds.com',
+    author=meta['author'],
+    author_email=meta['email'],
     url="https://github.com/ryankanno/py-golf",
     packages=packages,
     package_data={'': ['LICENSE']},
     package_dir={'py_golf': 'py_golf'},
     install_requires=requires,
-    license=py_golf.__license__,
+    license=meta['license'],
     tests_require=tests_require,
     classifiers=classifiers,
 )
