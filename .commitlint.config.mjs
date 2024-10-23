7de9c94392345c1a79b4b3f3dcee20ee6145cb42
Ryan Kanno <ryankanno@localkinegrinds.com>
2024-10-22 16:08:20 -1000

    chore: fixed commitlint and pre-commit

--- .commitlint.config.mjs
+++ .commitlint.config.mjs
@@ -1,10 +1,9 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
 
   rules: {
     // dependabot
     'header-max-length': [2, 'always', 200],
     'body-max-line-length': [0, 'always'],
-    'subject-case': [2, 'always', ['sentence-case', 'lower-case']]
   },
 };
